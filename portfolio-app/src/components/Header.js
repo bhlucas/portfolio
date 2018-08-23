96c801815e0deed3cd4dcadbc8f9254bebc0cdcf
to0p <benjamin.lucas.to0p@statefarm.com>
2018-08-23 13:30:41 -0700

    finished the navbar css to a stable begnng state

--- portfolio-app/src/components/Header.js
+++ portfolio-app/src/components/Header.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import styles from './Header.css'
 import logo from '../logo.svg';
-import ScrollReveal from 'scrollreveal';
-;
+
 
 class Header extends Component {
     constructor(props) {
@@ -11,7 +10,7 @@ class Header extends Component {
         this.state = {scrolledTop: true, loaded: false, currentScrollHeight: 0}
 
     }
-    componentDidMount() {0
+    componentDidMount() {
         window.addEventListener('scroll', this.handleScroll);
         this.setState({loading: true});
         window.onscroll =()=>{
@@ -20,7 +19,7 @@ class Header extends Component {
                 this.setState({currentScrollHeight: newScrollHeight})
             }
           }
-          ScrollReveal().reveal('.bg');
+
     }
     componentWillUnmount() {
         window.removeEventListener('scroll', this.handleScroll);
@@ -35,19 +34,19 @@ class Header extends Component {
 
     }
     render() {
-     const opacity = 1 - Math.min(200 / this.state.currentScrollHeight  , 1);
-
+     const opacity = 1;
+     console.log(currentScrollHeight);
       return ( 
-          
         <div>
-            <nav  className={"navbar navbar-expand-lg fixed-top " + (this.state.scrolledTop ? "" : "scrolled navbar-light bg-light")}>
-                <img src={logo} className={"navbar-brand slide-in logo "} alt="logo" />
+            <nav style={{opacity}} className={"navbar navbar-expand-lg fixed-top " + (this.state.scrolledTop ? "top" : "bg-light  navbar-light")}>
+                {/* <a class="navbar-brand" href="#">BHL</a> */}
+                <img src={logo} className={"navbar-brand swing slide-in"} alt="logo" />
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li class="nav-item active">
                             <a class="nav-link" href="#">Skills <span class="sr-only">(current)</span></a>
                         </li>
